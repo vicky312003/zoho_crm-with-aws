24705e3258cb53bfd5ccefe329589329165ff7be
vicky312003 <vignesh318m@gmail.com>
2025-05-07 21:09:46 +0530

    Update LoginController.java

--- src/main/java/com/Zoho_CRM_app_CB/Controller/LoginController.java
+++ src/main/java/com/Zoho_CRM_app_CB/Controller/LoginController.java
@@ -1,13 +1,1 @@
-package com.Zoho_CRM_app_CB.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";  // This refers to /WEB-INF/jsps/login.jsp
-    }
-}

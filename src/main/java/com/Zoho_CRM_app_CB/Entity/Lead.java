eb3a00afb5416f3f0a9f0f5294e3a7b753423a48
vicky312003 <vignesh318m@gmail.com>
2025-05-07 21:10:23 +0530

    Update Lead.java

--- src/main/java/com/Zoho_CRM_app_CB/Entity/Lead.java
+++ src/main/java/com/Zoho_CRM_app_CB/Entity/Lead.java
@@ -19,24 +19,7 @@ public class Lead{
 	@Column(name="Mobile",nullable=false,length=50)
 	private long mobile;
 	
-	@Column(nullable=false)
-	private String password;
-	@Column(nullable=false)
-	private String role;
 	
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
 	public long getId() {
 		return id;
 	}
